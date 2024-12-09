41d9939c02f3c96b8243451159d247400d4e026b
OsmarVillegas <osmarvm25@gmail.com>
2024-12-09 04:51:44 -0600

    Corrección Cache
    Corrección Cache

--- service-worker.js
+++ service-worker.js
@@ -32,8 +32,13 @@ const ASSETS = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching assets...");
-      return cache.addAll(ASSETS);
+      return Promise.all(
+        ASSETS.map((asset) =>
+          cache.add(asset).catch((err) => {
+            console.error(`Error al agregar ${asset} al caché:`, err);
+          })
+        )
+      );
     })
   );
 });
